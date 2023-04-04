5251b5013f7764d0d6f26b4240d597ad07ea6d02
kingsley <salvokingsley@gmail.com>
2023-04-04 14:35:49 -0700

    CORRECTION

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t nod = 0;
+ size_t num = 0;
 
  while (h)
  {
  printf("%d\n", h->n);
- nod++;
+ num++;
  h = h->next;
  }
 
- return (nod);
+ return (num);
 }
